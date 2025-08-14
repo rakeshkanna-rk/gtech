26f9f15e53107bf0c2cf41dd005aea9dc41e2731
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-14 15:46:27 +0530

    Added so it lists leads with dynamic

--- pages/leads.vue
+++ pages/leads.vue
@@ -1,5 +1,33 @@
-<template>
-  <div></div>
-</template>
+<script setup>
+import imageFrame from "~/components/LeadShowcase.vue";
+import monishImg from "~/assets/leadsPic/monish.JPG";
 
-<script setup></script>
+import devaImg from "~/assets/leadsPic/deva.JPG";
+
+const leadsData = [
+  {
+    id: 1,
+    name: "Monish",
+    image: monishImg,
+    about:
+      "Monish is the overall lead of the GTECH lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ",
+  },
+  {
+    id: 2,
+    name: "Deva",
+    image: devaImg,
+    about: "Deva is the overall co-lead of the GTECH",
+  },
+];
+</script>
+
+<template>
+  <imageFrame
+    v-for="(lead, index) in leadsData"
+    :key="index"
+    :leadName="lead.name"
+    :leadPic="lead.image"
+    :leadAbout="lead.about"
+    :align="index % 2 ? 'right' : 'left'"
+  />
+</template>
